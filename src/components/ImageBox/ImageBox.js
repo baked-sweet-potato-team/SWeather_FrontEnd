8cf6184daee79c0d8692714915aa140f6f9f5669
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-06-06 14:28:58 +0900

    이미지 삭제

--- src/components/ImageBox/ImageBox.js
+++ src/components/ImageBox/ImageBox.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './ImageBox.css';
 
 const ImageBox = (props) => {
+    
+    const SERVER_PORT = "5000";
+    let src = "여자코디/겨울/여자겨울로맨틱4.jpg";
+
+    console.log(props.imageRoute);
     return (
         <div id='w-img-box'>
-            {props.imageRoute}
-            <img id='codi-box' src='http://localhost:5000/남자코디/가을/남자가을베이직3.jpg'/>
+            <img id='codi-box' alt="이미지 없음" src={`http://localhost:${SERVER_PORT}/${src}`} />
         </div>
     );
 };
