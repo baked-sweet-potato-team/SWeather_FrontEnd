438961cf9e8f039bf779ae0427c237e3e029f174
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-06-06 14:47:48 +0900

    셀렉박스 성별에 따라 변경

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import MyHeader from '../../components/Header/Header'
 import './RegisterPage.css'
 
@@ -7,14 +8,15 @@ import {signupUser} from '../../_actions/user_action'
 
 function RegisterPage() {
 
-    const dispatch = useDispatch()
-    const [Email, setEmail] = useState("")
-    const [Password, setPassword] = useState("")
-    const [PasswordChk, setPasswordChk] = useState("")
-    const [Nickname, setNickname] = useState("")
-    const [Gender, setGender] = useState("")
-    const [Age, setAge] = useState("")
-    const [Style, setStyle] = useState("")
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordChk, setPasswordChk] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [gender, setGender] = useState("");
+    const [age, setAge] = useState("");
+    const [style, setStyle] = useState("");
 
     // handler
      const onEmailHandler = (e) => {
@@ -24,7 +26,7 @@ function RegisterPage() {
     const onPasswordHandler = (e) => {
         setPassword(e.currentTarget.value)
     }
-
+;
     const onPasswordCheckHandler = (e) => {
         setPasswordChk(e.currentTarget.value)
     }
@@ -49,18 +51,18 @@ function RegisterPage() {
         e.preventDefault()
 
         // 비번 일치 확인
-        if(Password !== PasswordChk){
+        if(password !== passwordChk){
             return alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.')
         }
 
          // 보낼 데이터
         let body = { 
-            email: Email,
-            password: Password,
-            nickname: Nickname,
-            gender: Gender,
-            age: Age,
-            style: Style
+            email: email,
+            password: password,
+            nickname: nickname,
+            gender: gender,
+            age: age,
+            style: style
         }
         console.log(body)
         // dispatch를 이용해서 signupUser라는 action 취하기
@@ -70,7 +72,7 @@ function RegisterPage() {
             console.log("회원가입",response);
             if(response.payload.success) {
                 // 회원가입 성공 시 로그인 화면 전환 
-                window.location.href = "/login"
+                navigate("/login");
             }
             else {
                 // 회원가입 실패 시 오류 출력
@@ -97,26 +99,26 @@ function RegisterPage() {
                                     <label className='r-text'>이메일</label>
                                     <div className='r-input-box'>
                                         <input className='r-inputs' type="email" placeholder=" 이메일을 입력하세요" 
-                                        value={Email} onChange={onEmailHandler}/>
+                                        value={email} onChange={onEmailHandler}/>
                                     </div>
                                     <label className='r-text'>비밀번호</label>
                                     <div className='r-input-box'>
                                         <input className='r-inputs' type="password" placeholder=" 비밀번호를 입력하세요" 
-                                        value={Password} onChange={onPasswordHandler}/>
+                                        value={password} onChange={onPasswordHandler}/>
                                     </div>
                                     <label className='r-text'>비밀번호 확인</label>
                                     <div className='r-input-box'>
                                         <input className='r-inputs' type="password" placeholder=" 비밀번호를 입력하세요" 
-                                        value={PasswordChk} onChange={onPasswordCheckHandler}/>
+                                        value={passwordChk} onChange={onPasswordCheckHandler}/>
                                     </div>
                                     <label className='r-text'>닉네임</label>
                                     <div className='r-input-box'>
                                         <input className='r-inputs' type="text" placeholder=" 닉네임을 입력하세요" 
-                                        value={Nickname} onChange={onNicknameHandler}/>
+                                        value={nickname} onChange={onNicknameHandler}/>
                                     </div>
                                     <label className='r-text'>성별</label>
                                     <div className='r-select-box'>
-                                        <select className='r-select' onChange={onGenderHandler} value={Gender}>
+                                        <select className='r-select' onChange={onGenderHandler} value={gender}>
                                             <option value="0" defaultValue>선택</option>
                                             <option value="남">남</option>
                                             <option value="여">여</option>
@@ -124,7 +126,7 @@ function RegisterPage() {
                                     </div>
                                     <label className='r-text'>연령대</label>
                                     <div className='r-select-box'>
-                                        <select className='r-select' onChange={onAgeHandler} value={Age}>
+                                        <select className='r-select' onChange={onAgeHandler} value={age}>
                                             <option value="0" defaultValue>선택</option>
                                             <option value="10">10대</option>
                                             <option value="20">20대</option>
@@ -133,16 +135,23 @@ function RegisterPage() {
                                     </div>
                                     <label className='r-text'>평소 스타일</label>
                                     <div className='r-select-box'>
-                                        <select className='r-select' onChange={onStyleHandler} value={Style}>
-                                            <option value="0" defaultValue>선택</option>
-                                            <option value="casual">casual</option>
-                                            <option value="street">street</option>
-                                            <option value="basic">basic</option>
-                                            <option value="campus">campus</option>
-                                            <option value="lovely">lovely</option>
-                                            <option value="romantic">romantic</option>
-                                            <option value="office">office</option>
-                                        </select>
+                                        {
+                                            gender === "여" ? 
+                                            <select className='r-select' onChange={onStyleHandler} value={style}>
+                                                <option value="0" defaultValue>선택</option>
+                                                <option value="basic">basic</option>
+                                                <option value="campus">campus</option>
+                                                <option value="romantic">romantic</option>
+                                                <option value="office">office</option>
+                                            </select> : 
+                                            <select className='r-select' onChange={onStyleHandler} value={style}>
+                                                <option value="0" defaultValue>선택</option>
+                                                <option value="formal">formal</option>
+                                                <option value="basic">basic</option>
+                                                <option value="campus">campus</option>
+                                                <option value="sports">sports</option>
+                                            </select>
+                                        }
                                     </div>
                                 </div>
                                 <div className='signup-btn-box'>
